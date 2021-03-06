4674ff7f39c22d99ddc0a156006c6befdd550d77
michaelklump <isaactj11@hotmail.com>
2021-03-06 21:27:05 +0800

    Update header.php

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
     
   </head>
   <body>
-    <header class="container-fluid herobg">
+  <header class="container-fluid herobg"<?php if ( get_field(' headerbackgroundimage ') ) { echo 'style="background: url(' . get_field(' headerbackgroundimage ') . ')"'; } ?>>
     <nav class="navbar">
         <div class="container-fluid">
           <div class="navbar-header">
@@ -60,6 +60,6 @@
 
       
         <div class="container">
-          <h1>Games Design For the Future</h1>
+        <h1><?php the_field('maintagline'); ?></h1>
         </div>
       </header>
