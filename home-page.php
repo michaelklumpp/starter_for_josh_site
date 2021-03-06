ad0fd2df9fd097d00dafce2e0b4896318d76873d
michaelklump <isaactj11@hotmail.com>
2021-03-06 21:14:52 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,36 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+?>
+
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+      <section class="container-fluid aboutbg text-center">
+        <div class="container">
+        
+
+          <div class="blocker"></div>
+        </div> 
+      </section>
+
+      <section class="container-fluid articlesbg">
+        <div class="container">
+          <div class="row">
+            <div>
+              <h3 class="text-center article-title">LATEST ARTICLES</h3>
+            </div>
+            <div class="col-md-4 article-cent">
+                     
+
+            </div>
+            <div class="col-md-4 article-cent">
+                       
+
+            </div>
+            <div class="col-md-4 article-cent">
+
+            </div>
+          </div>
+        </div>
+      </section>
+      <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
