9a5b11a24f387b5d21342084aa0fce0b327fe579
michaelklump <isaactj11@hotmail.com>
2021-03-06 21:27:09 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -8,6 +8,9 @@
       <section class="container-fluid aboutbg text-center">
         <div class="container">
         
+        <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+<p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+<p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
 
           <div class="blocker"></div>
         </div> 
@@ -16,19 +19,38 @@
       <section class="container-fluid articlesbg">
         <div class="container">
           <div class="row">
-            <div>
-              <h3 class="text-center article-title">LATEST ARTICLES</h3>
+          <div class="article-title text-center">
+            <h3 class="text-center article-title "><?php the_field('titleforarticles'); ?></h3>
             </div>
             <div class="col-md-4 article-cent">
-                     
-
+            <img class="img-responsive" alt=“portait” src="<?php the_field('articleimage'); ?>">
+                    <h4>
+                        <?php the_field('articletitle'); ?>
+                    </h4>
+                    <p>
+                        <?php the_field('articleexcerpt'); ?>
+                    </p>
+                <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
             </div>
             <div class="col-md-4 article-cent">
-                       
-
+            <img class="img-responsive" alt=“portait” src="<?php the_field('articleimage'); ?>">
+                    <h4>
+                        <?php the_field('articletitle'); ?>
+                    </h4>
+                    <p>
+                        <?php the_field('articleexcerpt'); ?>
+                    </p>
+                <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
             </div>
             <div class="col-md-4 article-cent">
-
+            <img class="img-responsive" alt=“portait” src="<?php the_field('articleimage'); ?>">
+                    <h4>
+                        <?php the_field('articletitle'); ?>
+                    </h4>
+                    <p>
+                        <?php the_field('articleexcerpt'); ?>
+                    </p>
+                <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
             </div>
           </div>
         </div>
