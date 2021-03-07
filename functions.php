5837e65b0116da1dada55012ec857eda84619368
michaelklump <isaactj11@hotmail.com>
2021-03-07 12:20:26 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -25,11 +25,29 @@ if($args->list_item_class){$classes[] = $args->list_item_class;
     }
 add_filter('nav_menu_css_class', 'add_menu_list_item_class', 1, 3);
  ?>
+
  <?php
 	if ( get_field(' headerbackgroundimage') ) {
 		echo 'style="background: url(' . get_field(' headerbackgroundimage') . ')"';
 	}
 ?>
+
+<?php
+function tagline() {
+
+	register_sidebar( array(
+		'name'          => 'main tagline',
+		'id'            => 'main_tag',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'tagline' );
+?>
+
 <?php
 add_action('wp_enqueue_scripts', 'sk_wow_init_in_footer');
 
